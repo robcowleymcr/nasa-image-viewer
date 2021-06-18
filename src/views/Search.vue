9d5e1034d0c9e2c1d022c6477e2f84b5b60cce51
robcowleymcr <robcowley06@hotmail.co.uk>
2021-06-18 12:50:25 +0100

    Added pagination to search page

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -4,13 +4,28 @@
       <b-row>
         <b-col cols="12">
           <h1 class="mb-4">NASA Search</h1>
-          <b-form-input class="mb-4" v-model="searchString" @input.native="onChange" placeholder="Search"></b-form-input>
+          <b-row class="justify-content-md-center">
+            <b-col cols="3">
+              <b-form-input width="200" class="mb-4" v-model="searchString" @input.native="onChange" placeholder="Search"></b-form-input>
+            </b-col>
+
+          </b-row>
         </b-col>
       </b-row>
       <b-row v-if="searchResults.length !== 0">
         <h2 class="mb-4">Results</h2>
-        <b-col v-for="(result, index) in searchResults" :key="index" cols="2">
+        <b-col cols="4" lg="3" xl="2" v-for="(result, index) in itemsForList" :key="index">
           <b-img @click="$router.push(`/asset/${result.data[0].nasa_id}`)" thumbnail :src="result.links[0].href" />
+        </b-col>
+        <b-col cols="12" class="pt-4">
+          <b-pagination
+            v-if="searchResults.length > 0"
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="perPage"
+            aria-controls="itemList"
+            align="center"
+          />
         </b-col>
       </b-row>
     </b-container>
@@ -26,6 +41,9 @@ export default {
   name: 'Search',
   data() {
     return {
+      currentPage: 0,
+      perPage: 10,
+      rows: 0,
       searchString: '',
       imageProps: {
           blankColor: '#bbb',
@@ -37,17 +55,24 @@ export default {
   computed: {
     ...mapState({
       searchResults: state => state.Search.searchResults
-    })
+    }),
+    itemsForList() {
+      return this.searchResults.slice(
+        (this.currentPage - 1) * this.perPage,
+        this.currentPage * this.perPage,
+      );
+    }
   },
   methods: {
     onChange(e) {
       const val = e.target.value;
       this.$store.dispatch('Search/performSearch', val)
+      .then(() => {
+        this.rows = this.searchResults.length;
+      })
     }
   },
-  created() {
-    // this.totalRows = this.searchResults.length;
-    // this.paginate(this.perPage, 0);
+  mounted() {
   }
 }
 </script>
